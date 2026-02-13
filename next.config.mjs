8eddb8f1668367bf98188e289d3f0acb4e3b6c56
emreakpinardev <emreakpinar46@gmail.com>
2026-02-13 21:57:39 +0300

    fix: serverExternalPackages for iyzipay build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  serverExternalPackages: ['iyzipay'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
